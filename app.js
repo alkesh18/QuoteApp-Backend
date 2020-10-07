76971355c2b149235c800a3b61027f8c7dd85453
Alkesh Sandal <alkesh18@hotmail.com>
2020-10-07 12:47:59 -0400

    Error handling and added nodemon

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const quoteRoutes = require('./api/routes/quotes');
 
-/* Middleware ============================================= */
-// Add to here as you add more endpoints
+app.use(morgan('dev'));
+
+/* Middleware - Routes ============================================= */
 app.use('/quotes', quoteRoutes);
+
+/* Error Handling ================================================== */
+app.use((req, res, next) => {
+    const error = new Error('Not found!');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 module.exports = app;
