6eeeadca0bd77a8e50e0b10a799ea990779c5ec5
Alkesh Sandal <alkesh18@hotmail.com>
2020-10-28 22:49:25 -0400

    signup and login with hashed passwords

--- api/models/user.js
+++ api/models/user.js
@@ -1,11 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    username: {type: String, required: true, unique: true },
-    password: {type: String, required: true },
-    role: {type: String, required: true },
-    active: {type: Boolean, required: true}
+  _id: mongoose.Schema.Types.ObjectId,
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, required: true },
+  active: { type: Boolean, required: true },
 });
 
-module.exports = mongoose.model('User', userSchema);
+userSchema.pre("save", function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) return next();
+
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function (candidatePassword, cb) {
+  return bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+module.exports = mongoose.model("User", userSchema);
--- api/routes/user.js
+++ api/routes/user.js
@@ -23,19 +23,16 @@ router.get("/", (req, res, next) => {
 });
 
 // Create a user
-router.post("/register", (req, res, next) => {
-  bcrypt
-    .hash(req.body.password, 10)
-    .then((hash) => {
-      const user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        username: req.body.username,
-        password: hash,
-        role: req.body.role,
-        active: req.body.active,
-      });
-      return user.save();
-    })
+router.post("/signup", (req, res, next) => {
+  const user = new User({
+    _id: new mongoose.Types.ObjectId(),
+    username: req.body.username,
+    password: req.body.password,
+    role: req.body.role,
+    active: req.body.active,
+  });
+  user
+    .save()
     .then((result) => {
       console.log(result);
       res.status(200).json(result);
@@ -52,24 +49,31 @@ router.get("/login", (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  User.findOne({username})
+  User.findOne({ username })
     .exec()
     .then((user) => {
-      console.log(user);
       if (user) {
-        bcrypt.compare(password, user.password, (err, isMatch) => {
-            if(isMatch) {
-                res.status(200).json({
-                    username: user.username,
-                    role: user.role,
-                    active: user.active,
-                });
+        user.comparePassword(password,  (err, isMatch) => {
+          if (isMatch) {
+            console.log(`${user.username} authenicated`);
+            if (user.active) {
+              res.status(200).json({
+                username: user.username,
+                role: user.role,
+                active: user.active,
+                authenticated: true
+              });
             } else {
-                res.status(404).json({ message: "Invalid credentials" });
+              res.status(404).json({ message: "Inactive user", error: err });
             }
-        })
+          } else {
+            res
+              .status(404)
+              .json({ message: "Invalid credentials", error: err });
+          }
+        });
       } else {
-          res.status(500).json({message: "Invalid username"})
+        res.status(500).json({ message: "Invalid username" });
       }
     })
     .catch((err) => {
@@ -89,36 +93,50 @@ router.get("/login", (req, res, next) => {
     ]
 }
 */
-router.patch("/updateUser", (req, res, next) => {
-  const userId = req.body.params.userId;
-  const updateOps = {};
-  for (const ops of req.body.params.operations) {
-    updateOps[ops.propName] = ops.value;
-  }
-  User.update({ _id: userId }, { $set: updateOps })
-    .exec()
-    .then((result) => res.status(200).json(result))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
-});
+// router.patch("/updateUser", (req, res, next) => {
+//   const userId = req.body.params.userId;
+//   const updateOps = {};
+//   for (const ops of req.body.params.operations) {
+//     updateOps[ops.propName] = ops.value;
+//   }
+//   User.update({ _id: userId }, { $set: updateOps })
+//     .exec()
+//     .then((result) => res.status(200).json(result))
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json({
+//         error: err,
+//       });
+//     });
+// });
 
-router.delete("/deleteUser", (req, res, next) => {
-  const userId = req.body.userId;
-  User.remove({ _id: userId })
+router.patch("/disableUser", (req, res, next) => {
+  const userId = req.body.userid;
+  User.update({ _id: userId }, { $set: { active: false } })
     .exec()
-    .then((result) => {
-      res.status(200).json(result);
-    })
+    .then((result) =>
+      res.status(200).json({
+        message: "User was disabled",
+        result: result,
+      })
+    )
     .catch((err) => {
       console.log(err);
       res.status(500).json({
         error: err,
       });
     });
+  // User.remove({ _id: userId })
+  //   .exec()
+  //   .then((result) => {
+  //     res.status(200).json(result);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //     res.status(500).json({
+  //       error: err,
+  //     });
+  //   });
 });
 
 module.exports = router;
